2d75c9156f950b7c4cd86f920d5a21f802b53acd
marthamwangi <martyturing@gmail.com>
2022-07-01 20:46:26 +0300

    using data-testid

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ export default function App() {
   return (
     <div className="App">
       learn react
+      <a href='google.com'
+        rel='noopener noreferrer'
+        data-testid='go-google'>Google</a>
     </div>
   );
 }
